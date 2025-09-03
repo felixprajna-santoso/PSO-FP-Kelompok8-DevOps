6ebdfadcaf8294499dab53e0e451aa00128f1bcd
Thinal <r.thinal.fernando@gmail.com>
2025-09-03 18:43:39 +0530

    Added SQLAlchemy setup and Info model for employee management

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
 
 
 
 app = Flask(__name__)
+app.secret_key = "123"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employees.db'  #change to mysql db in future
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+
+db = SQLAlchemy(app)
+
+
+class Info(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(200))
+    email = db.Column(db.String(200))
+    phone = db.Column(db.String(200))
+
+    def __init__(self, name, email,phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
+
 
 @app.route('/')
 def Index():
