8e779a92e8f48bf8ea0b97f2de43cf6b12a959a8
Thinal <r.thinal.fernando@gmail.com>
2025-09-03 20:55:11 +0530

    Implemented update functionality for employee records and modified the edit modal to pass the correct employee data.

--- app.py
+++ app.py
@@ -45,7 +45,18 @@ def insert():
 
         return redirect(url_for('Index'))
 
+@app.route('/update', methods = ['GET', 'POST'])
+def update():
+    if request.method == 'POST':
+        my_data = Info.query.get(request.form.get('id'))
+        my_data.name = request.form['name']
+        my_data.email = request.form['email']
+        my_data.phone = request.form['phone']
 
+        db.session.commit()
+        flash("Employee Updated Successfully")
+
+        return redirect(url_for('Index'))
 
 
 if __name__ == "__main__":
