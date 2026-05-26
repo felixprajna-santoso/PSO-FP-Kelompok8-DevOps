35a41a7a9afb183085c52037ec319b394f2bec18
fachry2005 <frssfachry@gmail.com>
2026-05-26 21:31:19 +0700

    feat: setup CI/CD pipeline, Docker, and IaC

--- app.py
+++ app.py
@@ -1,36 +1,32 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
-
-
 app = Flask(__name__)
 app.secret_key = "123"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employees.db'  #change to mysql db in future
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employees.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-
 class Info(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200))
     email = db.Column(db.String(200))
     phone = db.Column(db.String(200))
 
-    def __init__(self, name, email,phone):
+    def __init__(self, name, email, phone):
         self.name = name
         self.email = email
         self.phone = phone
 
-
 @app.route('/')
 def Index():
-    all_data = Info.query.all( )
+    all_data = Info.query.all()
 
-    return render_template("index.html", employees = all_data)
+    return render_template("index.html", employees=all_data)
 
-@app.route('/insert', methods = ['POST'])
+@app.route('/insert', methods=['POST'])
 def insert():
     if request.method == "POST":
         name = request.form['name']
@@ -45,10 +41,11 @@ def insert():
 
         return redirect(url_for('Index'))
 
-@app.route('/update', methods = ['GET', 'POST'])
+@app.route('/update', methods=['GET', 'POST'])
 def update():
     if request.method == 'POST':
         my_data = Info.query.get(request.form.get('id'))
+
         my_data.name = request.form['name']
         my_data.email = request.form['email']
         my_data.phone = request.form['phone']
@@ -58,15 +55,19 @@ def update():
 
         return redirect(url_for('Index'))
 
-
-@app.route('/delete/<id>', methods = ['GET', 'POST'])
+@app.route('/delete/<id>', methods=['GET', 'POST'])
 def delete(id):
     my_data = Info.query.get(id)
+
     db.session.delete(my_data)
     db.session.commit()
+
     flash("Employee Deleted Successfully")
 
     return redirect(url_for('Index'))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+
+    app.run(host="0.0.0.0", port=5000, debug=True)
